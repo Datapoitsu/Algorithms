36fd3ff6d9cdccce259056c842cbbc9a6affe10a
Thaumatized <veeti.junkkala@gmail.com>
2025-04-03 19:21:28 +0300

    Fibonacci improvement + js

---
+++ JavaScript/Math/Sequences/fibonacci.js
@@ -0,0 +1,1 @@
+fibonacci = (n,i = 1,h = [0,1]) => i < n ? fibonacci(n,i+1,[h[1],h[0]+h[1]]) : h[n-i+1]
--- Python/Math/Sequences/fibonacci.py
+++ Python/Math/Sequences/fibonacci.py
@@ -1,1 +1,1 @@
-fibonacci = lambda n,i = 1,h = [0,1] : fibonacci(n,i+1,[h[1],h[0]+h[1]]) if i < n else h[n-i-1]
+fibonacci = lambda n,i = 1,h = [0,1] : fibonacci(n,i+1,[h[1],h[0]+h[1]]) if i < n else h[n-i+1]
